98ab0f2e84c2bcf7118d45212f2f5ede988838d4
140ch204 <140ch204_github@gadz.org>
2019-10-01 23:33:49 +0200

    V1

--- exo_21bis.rb
+++ exo_21bis.rb
@@ -1,22 +1,23 @@
 
 puts "Combien d'étage veux-tu ? entre 1 et 25 "
 nb_floor = gets.chomp.to_i
+if (nb_floor > 0 && nb_floor < 26) then 
+    i = 1
+    while (i <= nb_floor) do
 
-i = 1
-while (i <= nb_floor) do
+        j = 1
+        while (j <= nb_floor-i) do
+            print " "
+            j +=1
+        end
 
-    j = 1
-    while (j <= nb_floor-i) do
-        print " "
-        j +=1
+        j=1
+        while (j <= i) do
+            print "#"
+            j +=1
+        end
+        print "\n"
+
+        i +=1
     end
-
-    j=1
-    while (j <= i) do
-        print "#"
-        j +=1
-    end
-    print "\n"
-
-    i +=1
-end
+end 
